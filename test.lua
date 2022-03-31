79024a22b84b725644d54a0782243cfbc8d2f6d7
Venkatesh Mishra <100060154+vmishra103269@users.noreply.github.com>
2022-03-31 11:16:03 +0530

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,12 @@
+-- (C) 2022 Venkatesh Mishra
+
+function main()
+  print("gcode compiling enine v0.1")
+  --os.execute("tcc") -- ANSI C99
+  --os.execute("clang") -- C++
+  --os.execute("") -- Python3
+  --os.execute("") -- Rust
+  --os.execute("") -- Go
+end
+
+main()
